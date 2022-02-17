e659f527b20edba7f06a129b9af4d71405fdbba7
djambazov <filip.dzambazov@telekom.mk>
2022-02-17 15:48:10 +0100

    fixed what i broke :)

--- src/main/java/TaskDB.java
+++ src/main/java/TaskDB.java
@@ -105,5 +105,33 @@ public class TaskDB {
         return tasks;
     }
 
+    public List<Task> getAllTasksFilterByTitle(String filter) throws SQLException {
+
+        List<Task> tasks = new ArrayList<>();
+
+        String sql = "SELECT * FROM tasks WHERE title LIKE ? ORDER BY title";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1, "%" + filter + "%");
+        ResultSet result = preparedStatement.executeQuery();
+
+        while (result.next()) {
+            Task task = new Task(
+                    result.getLong("id"),
+                    result.getLong("category_id"),
+                    result.getString("title"),
+                    result.getString("description"),
+                    result.getTimestamp("created_date"),
+                    result.getTimestamp("due_date"),
+                    result.getBoolean("done")
+            );
+
+            tasks.add(task);
+        }
+
+        return tasks;
+    }
+
 
 }
